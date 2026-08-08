b513a1e302f2d00415c3b7aadefa5f68c85f4727
xaviermercado <ing.fxmm@gmail.com>
2026-08-07 22:21:43 -0700

    Restore mobile viewport metadata

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -8,6 +8,7 @@ export default function Root({ children }: PropsWithChildren) {
     <html {...htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         {headNodes}
       </head>
       <body {...bodyAttributes}>
